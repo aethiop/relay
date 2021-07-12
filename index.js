89ca953f3205e0606607da1836ffcd254b22c01b
Natnael Teferi <pinobirhanu3@gmail.com>
2021-07-12 02:59:54 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const Gun = require("gun");
+const app = express();
+const port = 8765;
+app.use(Gun.serve);
+
+const server = app.listen(port, () => {
+	console.log(`App listening at http://localhost:${port}`);
+});
+
+Gun({ web: server });
